36b61c4a3a97043a14ead3667adcb00ab1c90d84
NgoManhJP <ngovanmanh88@gmail.com>
2020-02-08 07:00:38 +0900

    Update App

--- iris_eda_app.py
+++ iris_eda_app.py
@@ -1,9 +1,26 @@
 import streamlit as st 
 
 def main():
-    """ A simple Iris EDA App """
-    st.title("Iris EDA App with streamlit")
-    st.header("Streamlit is Cool")
+	"""Deploying Streamlit App with Docker"""
+
+	st.title("Streamlit App")
+	st.header("Deploying Streamlit with Docker on GCP")
+
+	st.title("Updated Version")
+
+
+	activities = ["EDA","Plots"]
+
+	choices = st.sidebar.selectbox('Select Activities',activities)
+
+	if choices == 'EDA':
+		st.subheader("EDA")
+
+	elif choices == 'Plots':
+		st.subheader("Visualization")
+
+
+
 
 if __name__ == '__main__':
-	main()    
+	main()
