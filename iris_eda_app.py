071daa58bd3c433dd3aba4b23126ff3e66d632b5
NgoManhJP <ngovanmanh88@gmail.com>
2020-02-08 06:36:06 +0900

    Iris EDA app deployment

---
+++ iris_eda_app.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+def main():
+    """ A simple Iris EDA App """
+    st.title("Iris EDA App with streamlit")
+    st.header("Streamlit is Cool")
+
+if __name__ == '__main__':
+	main()    
